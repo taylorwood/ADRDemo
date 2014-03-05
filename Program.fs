98d4ac7f4756025bbe07d35054521e2f5a2aa50f
Taylor Wood <taylorwood@me.com>
2014-03-05 10:49:03 -0600

    Added a few comments

--- Program.fs
+++ Program.fs
@@ -7,13 +7,16 @@ module Console =
 
     [<EntryPoint>]
     let main argv =
-        let adrPath = @"C:\Temp\ADR\Demo\FirstPageTrainingData"
+        //the training data is expected to be .txt files organized into folders by category/class
+        //the folder name is the category name
+        let adrPath = @"C:\Temp\ADR\Demo\FirstPageTrainingData\"
 
+        //load, tokenize, weight all training data samples
         let trainingData, trainedData, idfMap = loadTrainingData adrPath
 
-        let queryPath = adrPath + @"\URLA\20173_URLA.txt"
+        let queryTextPath = adrPath + "unknown.txt" //path to the "query" document to be classified/categorized
         let tfidfTextFile = tokenizeFile >> getTermFreqs >> weightTermFreqs idfMap >> Set.ofSeq
-        let weightedQuery = tfidfTextFile queryPath
+        let weightedQuery = tfidfTextFile queryTextPath
 
         let probabilities = calcCategoryProbabilities trainedData weightedQuery |> Seq.truncate 10
         printfn "Top 10 matches: %A" probabilities
